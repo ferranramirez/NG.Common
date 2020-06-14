eb2b1923f01deedcd9be25a4087fce3a73622405
ferranramirez <ferranramireznavajon@gmail.com>
2020-06-14 10:20:00 +0200

    Update healthcheck ui extension

--- src/NG.Common.Library/Extensions/HealthCheckExtensions.cs
+++ src/NG.Common.Library/Extensions/HealthCheckExtensions.cs
@@ -9,14 +9,15 @@ namespace NG.Common.Library.Extensions
 {
     public static class HealthCheckExtensions
     {
-
         public static void AddHealthCheckMiddleware(
             this IServiceCollection services,
             IConfiguration config)
         {
+            var baseUrl = config.GetSection("Urls").GetValue<string>("Base") ?? "localhost:80";
+            var hcName = string.Concat(config.GetSection("Documentation").GetValue<string>("Title"), "HealthCheck");
             services.AddHealthChecks()
                     .AddSqlServer(config.GetConnectionString("NotGuiriDb"));
-            services.AddHealthChecksUI(setup => setup.AddHealthCheckEndpoint("API HealthCheck", "/health"))
+            services.AddHealthChecksUI(setup => setup.AddHealthCheckEndpoint(hcName, string.Concat(baseUrl, "/health")))
                     .AddInMemoryStorage();
         }
 
